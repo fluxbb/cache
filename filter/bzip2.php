2f02cbf4b6b30341301633ba006bdfb2108aa84c
Jamie Furness <jamie@jamierf.co.uk>
2010-08-14 11:41:09 +0100

    Adding a BZip2 filter

---
+++ filter/bzip2.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+* The BZip2 filter compresses data using BZip2. BZip2 can reach a higher
+* compression ratio than GZip but is considerabily slower.
+* http://uk2.php.net/manual/en/book.bzip2.php
+* 
+* Copyright (C) 2010 Jamie Furness
+* License: http://www.gnu.org/licenses/gpl.html GPL version 3 or higher
+*/
+
+class Filter_BZip2 implements Filter
+{
+	private $level;
+
+	/**
+	* Initialise a new BZip2 filter.
+	* 
+	* @param	level	The compression level to use, ranging from 0-9
+	*/
+	public function __construct($config)
+	{
+		$this->level = $config['level'];
+	}
+
+	public function encode($data)
+	{
+		return bzcompress($data, $this->level);
+	}
+
+	public function decode($data)
+	{
+		return bzdecompress($data);
+	}
+}
