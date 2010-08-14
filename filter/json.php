b4dd24fd968bdd9c7a691f6c1b368a7a0eb0f7da
Frank <frank@61924.nl>
2010-08-14 18:34:35 +0800

    Added Json filter.

---
+++ filter/json.php
@@ -0,0 +1,19 @@
+<?php
+
+class Filter_JSON implements Filter
+{
+	public function __construct($config)
+	{
+
+	}
+
+	public function encode($data)
+	{
+		return json_encode($data);
+	}
+
+	public function decode($data)
+	{
+		return json_decode($data, true);
+	}
+}
