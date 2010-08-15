23ece5ae949bf061f5b07f7f2366865cbbc42d7e
Jamie Furness <jamie@jamierf.co.uk>
2010-08-15 21:08:24 +0100

    Adding TTL emulation to the APC layer. APC does support TTLs however not within a single session. I cant think of any use-case when this would matter, however we want all layers to behave the same...

--- cache/cache/apc.php
+++ cache/cache/apc.php
@@ -19,10 +19,41 @@ class Cache_APC extends Cache
 			throw new Exception('The APC cache requires the APC extension.');
 	}
 
+	// Since we are emulating the TTL we need to override set()
+	public function set($key, $data, $ttl = 0)
+	{
+		// APC does support TTL however not within a single session so lets emulate it
+		$data = array('expire' => $ttl > 0 ? time() + $ttl : 0, 'data' => $data);
+
+		parent::set($key, $data, $ttl);
+	}
+
 	protected function _set($key, $data, $ttl)
 	{
 		if (apc_store($key, $data, $ttl) === false)
 			throw new Exception('Unable to write APC cache: '.$key);
+	}
+
+	// Since we are emulating the TTL we need to override get()
+	public function get($key)
+	{
+		$data = parent::get($key);
+		if ($data === self::NOT_FOUND)
+			return self::NOT_FOUND;
+
+		// Check if the data has expired
+		if ($data['expire'] > 0 && $data['expire'] < time())
+		{
+			$this->delete($key);
+
+			// Correct the hit/miss counts
+			$this->hits--;
+			$this->misses++;
+
+			return self::NOT_FOUND;
+		}
+
+		return $data['data'];
 	}
 
 	protected function _get($key)
