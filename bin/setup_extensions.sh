bbb03d0957e2a15d12d2da3a60b7bfa0d6beb71e
Franz Liedke <franz@develophp.org>
2012-02-14 17:34:47 +0100

    Use proper package for downloading Redis extension.

--- bin/setup_extensions.sh
+++ bin/setup_extensions.sh
@@ -21,7 +21,7 @@ echo "apc.enabled=1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:
 echo "apc.enable_cli=1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 # Install Redis bindings
-wget https://github.com/downloads/nicolasff/phpredis/php_redis-5.3-vc9-ts-73d99c3e.zip -O phpredis.zip
-unzip phpredis.zip
-sh -c "cd phpredis && phpize && ./configure && make && sudo make install"
+wget https://github.com/nicolasff/phpredis/tarball/2.1.3 -O phpredis.tgz
+tar -xzf phpredis.tgz
+sh -c "cd nicolasff-phpredis-43bc590 && phpize && ./configure && make && sudo make install"
 echo "extension=redis.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
