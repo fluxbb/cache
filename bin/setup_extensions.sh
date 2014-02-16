2cf339397fd74dbb9098ff2c65d87f86625cb437
Jamie Furness <jfurness@yammer-inc.com>
2014-02-16 17:58:36 +0000

    Removing some cache extensions that require external services

--- bin/setup_extensions.sh
+++ bin/setup_extensions.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 # Enable Memcache
-echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+#echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install Memcached
-echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+#echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install APC
 echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
@@ -12,7 +12,7 @@ echo "apc.enabled = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install Redis bindings
-echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+#echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install YAML
 pecl install yaml
