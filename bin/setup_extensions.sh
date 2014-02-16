6a22dd1ee858bf80d5a43007031ac30a1189604d
Jamie Furness <jfurness@yammer-inc.com>
2014-02-16 17:58:17 +0000

    simplying installing of a bunch of extensions

--- bin/setup_extensions.sh
+++ bin/setup_extensions.sh
@@ -1,43 +1,23 @@
 #! /bin/sh
 
-# Install Memcache
-wget http://pecl.php.net/get/memcache-2.2.6.tgz
-tar -xzf memcache-2.2.6.tgz
-sh -c "cd memcache-2.2.6 && phpize && ./configure --enable-memcache && make && sudo make install"
-echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+# Enable Memcache
+echo "extension = memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install Memcached
-wget http://pecl.php.net/get/memcached-1.0.2.tgz
-tar -xzf memcached-1.0.2.tgz
-sh -c "cd memcached-1.0.2 && phpize && ./configure && make && sudo make install"
-echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install APC
-wget http://pecl.php.net/get/APC-3.1.9.tgz
-tar -xzf APC-3.1.9.tgz
-sh -c "cd APC-3.1.9 && phpize && ./configure && make && sudo make install"
-echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-echo "apc.enabled=1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-echo "apc.enable_cli=1" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "apc.enabled = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install Redis bindings
-wget https://github.com/nicolasff/phpredis/tarball/2.1.3 -O phpredis.tgz
-tar -xzf phpredis.tgz
-sh -c "cd nicolasff-phpredis-43bc590 && phpize && ./configure && make && sudo make install"
-echo "extension=redis.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "extension = redis.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 # Install YAML
-wget http://pecl.php.net/get/yaml-1.1.0.tgz
-tar -xzf yaml-1.1.0.tgz
-sh -c "cd yaml-1.1.0 && phpize && ./configure && make && sudo make install"
-echo "extension=yaml.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-
-# Install Pear::XML_Serializer
-pyrus install pear/XML_Serializer-0.20.2
-phpenv rehash
+pecl install yaml
 
 # Install LZF
-wget http://pecl.php.net/get/LZF-1.5.2.tgz
-tar -xzf LZF-1.5.2.tgz
-sh -c "cd LZF-1.5.2 && phpize && ./configure && make && sudo make install"
-echo "extension=lzf.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+pecl install lzf
+
+phpenv rehash
